dd63c9cafe4cb492b1bb6153f05bc17f46e31063
Tchuv <tchuvashov1706@gmail.com>
2024-04-08 10:26:51 +1200

    создал два дочерних класса

--- 1 lesson/DZ/dz.py
+++ 1 lesson/DZ/dz.py
@@ -23,7 +23,7 @@ class Product:
 
     @price.setter
     def price(self, new_price):
-        if new_price <=0:
+        if new_price <= 0:
             print('Некорректная цена')
         else:
             self.__price = new_price
@@ -35,27 +35,25 @@ class Product:
         return self.__price * self.quantity + other.price * other.quantity
 
 
-
 class Category:
     ''' класс Category'''
-    name: str #имя
-    description: str #описание
-    products: list #список продуктов в категории
-    all_category = 0 #количество категорий
-    all_products = [] #список всех уникальных продуктов
-    quantity_product = 0 #общее количествоуникальных продуктов
-
+    name: str  # имя
+    description: str  # описание
+    products: list  # список продуктов в категории
+    all_category = 0  # количество категорий
+    all_products = []  # список всех уникальных продуктов
+    quantity_product = 0  # общее количествоуникальных продуктов
 
     def __init__(self, name, description, products):
         '''Инициализация обьекта Category'''
         self.name = name
         self.description = description
-        self. __products = products
+        self.__products = products
         Category.all_category += 1
-        for i in range(len(self.__products)): #добавление новых уникальных продуктов
+        for i in range(len(self.__products)):  # добавление новых уникальных продуктов
             if not self.__products[i] in Category.all_products:
                 Category.all_products.append(self.__products[i])
-        Category.quantity_product = len(Category.all_products) #количество уникальных продуктов
+        Category.quantity_product = len(Category.all_products)  # количество уникальных продуктов
 
     def add_products(self, value):
         self.__products.append(value)
@@ -70,8 +68,42 @@ class Category:
     def __str__(self):
         return f'{self.name}, количество продуктов: {Category.quantity_product} шт.'
 
-a = Category('шарики', 'шары надувные', [['красный', 'шарик резиновый', 3.5, 5], ['белый','шарик силиконовый', 5.5, 7]])
+class Smartphone(Product):
+    """ дочерний класс телефоны"""
+    def __init__(self, name: str, description: str, price: float, quantity: int,
+                 performance: float, model: str, memory: int, color: str):
+        super().__init__(name, description, price, quantity)
+        self.performance = performance
+        self.model = model
+        self.memory = memory
+        self.color = color
+
+class Lawn_grass(Product):
+    """Дочерний класс Газонная трава"""
+    def __init__(self, name: str, description: str, price: float, quantity: int,
+                 country:str, germ_period: int, color: str):
+        super().__init__(name, description, price, quantity)
+        self.country = country
+        self.germ_period = germ_period
+        self.color = color
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+a = Category('шарики', 'шары надувные',
+             [['красный', 'шарик резиновый', 3.5, 5], ['белый', 'шарик силиконовый', 5.5, 7]])
 b = Category('кубики', 'кубы деревянные', ["'тяжёлый', 'куб свинцовый, 7, 2", "'легкий','куб деревянный', 2, 10"])
-c = Product('шарик1','шарик резиновый', 3.5, 10)
+c = Product('шарик1', 'шарик резиновый', 3.5, 10)
 
 
