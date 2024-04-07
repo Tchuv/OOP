3523ce7ae1c15dad5ac349378abaecddce021006
Tchuv <tchuvashov1706@gmail.com>
2024-04-08 11:05:46 +1200

    исправил ошибку

--- 1 lesson/DZ/dz.py
+++ 1 lesson/DZ/dz.py
@@ -58,7 +58,7 @@ class Category:
         Category.quantity_product = len(Category.all_products)  # количество уникальных продуктов
 
     def add_products(self, value):
-        if not isinstance((value, Product):
+        if isinstance(value, Product ):
             raise TypeError('Ошибка. Обьект другого типа')
         self.__products.append(value)
 
